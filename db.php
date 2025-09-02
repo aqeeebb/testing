eda4b39b325a51dbe9f88c3238d2f0355b21c228
aqeeebb <aqeeebb@gmail.com>
2025-09-02 14:38:45 +0500

    Create db.php

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+$servername = getenv("DB_HOST") ?: "localhost";
+$username   = getenv("DB_USER") ?: "root";
+$password   = getenv("DB_PASS") ?: "";
+$database   = getenv("DB_NAME") ?: "taskdb";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
